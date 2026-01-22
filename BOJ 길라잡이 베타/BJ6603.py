a336f04524bb7509f4f265a6870faf8f9d3c6eee
junyong lee <71388422+Jun-yong-lee@users.noreply.github.com>
2026-01-22 12:13:23 +0900

    Add | dfs(Backtracking) code

--- BOJ 길라잡이 베타/BJ6603.py
+++ BOJ 길라잡이 베타/BJ6603.py
@@ -15,12 +15,34 @@ from itertools import combinations
 
 input = sys.stdin.readline
 
+def combi(iter, r):
+    arr = list(iter)
+    n = len(arr)
+    result = []
+    path = []
+
+    def dfs(start):
+        if len(path) == r:
+            result.append(tuple(path))
+            return
+        
+        for i in range(start, n):
+            path.append(arr[i])
+            dfs(i + 1)
+            path.pop()
+
+    dfs(0)
+    return result
+
 while True:
     s = input().rstrip()
     if s == "0":
         break
     else:
         nums = list(map(int, s.split()))[1:]
-        for i in combinations(nums, 6):
+        for i in combi(nums, 6):
             print(" ".join(map(str, i)))
+
+        # for i in combinations(nums, 6):
+        #     print(" ".join(map(str, i)))
         print()
