e27ac96d8fb4a14fa71a46f59cd3e9f13302c827
junyong lee <71388422+Jun-yong-lee@users.noreply.github.com>
2026-01-27 17:03:28 +0900

    BOJ's 5086 problem solved code

---
+++ 단계별/BJ5086.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+while True:
+    A, B = map(int, input().split())
+    if A == B == 0:
+        break
+
+    if A % B == 0:
+        print("multiple")
+    elif B % A == 0:
+        print("factor")
+    else:
+        print("neither")
