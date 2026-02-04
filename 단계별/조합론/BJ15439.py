a547324f506bb63df40de41f1ec9e21b67c91833
junyong lee <71388422+Jun-yong-lee@users.noreply.github.com>
2026-02-04 15:22:31 +0900

    BOJ's 15439 problem solved code

---
+++ 단계별/조합론/BJ15439.py
@@ -0,0 +1,11 @@
+import sys
+from itertools import permutations
+
+N = int(input())
+count = 0
+
+for i in range(N):
+    for j in range(N):
+        if i != j:
+            count += 1
+print(count)
