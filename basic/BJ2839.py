19a9df4bac260ab3c9a03680b085ca7e44d0a8f8
JunyongLee <wnsdyd1569@naver.com>
2026-03-30 00:20:28 +0900

    BOJ's 2839 problem solved code using greedy algorithm

--- basic/BJ2839.py
+++ basic/BJ2839.py
@@ -1,4 +1,4 @@
-# 그리디 문제
+# 완전 탐색
 import sys
 input = sys.stdin.readline
 
@@ -14,3 +14,23 @@ if len(res) == 0:
     print("-1")
 else:
     print(min(res))
+
+# 그리디
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+
+bags = 0
+flag = False
+
+while N >= 0:
+    if N % 5 == 0:
+        bags += (N // 5)
+        flag = True
+        print(bags)
+        break
+    N -= 3
+    bags += 1
+if flag == False:
+    print("-1")
